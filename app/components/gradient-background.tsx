67632f19619b12e82064aed28e696f7e16c96d49
Ben Chavez <ben.m.chavez@gmail.com>
2023-07-14 23:23:34 -0500

    updated background to remove overflow

--- app/components/gradient-background.tsx
+++ app/components/gradient-background.tsx
@@ -1,34 +1,5 @@
-// import { useEffect } from 'react';
-// import { Gradient } from '../../public/gradient';
-
-// function GradientBackground() {
-//   useEffect(() => {
-//     // Create your instance
-//     const gradient = new Gradient();
-
-//     // Call `initGradient` with the selector to your canvas
-//     gradient.initGradient('#gradient-canvas');
-//   }, []);
-
-//   return (
-//     <div>
-//       <style jsx>{`
-//         #gradient-canvas {
-//           width: 100%;
-//           height: 100%;
-//           --gradient-color-1: #c3e4ff;
-//           --gradient-color-2: #6ec3f4;
-//           --gradient-color-3: #eae2ff;
-//           --gradient-color-4: #b9beff;
-//         }
-//       `}</style>
-//       <canvas id="gradient-canvas" data-transition-in />
-//     </div>
-//   );
-// }
-// @ts-nocheck
-// export default GradientBackground;
 'use client';
+
 import { useEffect, useState } from 'react';
 
 function GradientBackground() {
@@ -36,10 +7,8 @@ function GradientBackground() {
 
   useEffect(() => {
     import('../../public/gradient').then(({ Gradient }) => {
-      // Create your instance
       const gradient = new Gradient();
 
-      // Call `initGradient` with the selector to your canvas
       gradient.initGradient('#gradient-canvas');
 
       setGradient(gradient);
@@ -48,31 +17,8 @@ function GradientBackground() {
 
   return (
     <div>
-      {/* <style jsx>{`
-        #gradient-canvas {
-          width: 100%;
-          height: 100%;
-          --gradient-color-1: #c3e4ff;
-          --gradient-color-2: #6ec3f4;
-          --gradient-color-3: #eae2ff;
-          --gradient-color-4: #b9beff;
-        }
-      `}</style> */}
-      <style jsx>{`
-        #gradient-canvas {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100vw;
-          height: 100vh;
-          transform: rotate(180deg);
-          --gradient-color-1: #c3e4ff;
-          --gradient-color-2: #6ec3f4;
-          --gradient-color-3: #eae2ff;
-          --gradient-color-4: #b9beff;
-        }
-      `}</style>
-      <canvas id="gradient-canvas" data-transition-in />
+      {/* rotate-180 */}
+      <canvas id="gradient-canvas" data-transition-in className="absolute" />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import { Inter, Open_Sans } from 'next/font/google';
-import './globals.css';
-import Navbar from './components/layout/navbar';
-import Footer from './components/layout/footer';
-import TestComponent from './components/test-component';
-import HeroAnimation from './components/icons/hero-animation';
 import GradientBackground from './components/gradient-background';
+import Footer from './components/layout/footer';
+import Navbar from './components/layout/navbar';
+import './globals.css';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -14,7 +12,6 @@ const inter = Inter({
 
 const open_sans = Open_Sans({
   subsets: ['latin'],
-  // display: 'swap',
   variable: '--font-open-sans',
 });
 
@@ -31,15 +28,23 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      // h-full
       className={`${inter.variable} ${open_sans.variable} scroll-smooth antialiased`}
     >
       <body className="debug-screens relative mx-auto">
-        <GradientBackground
-        // className="w-full h-full"
-        />
-        <div className="absolute inset-0 h-full w-full overflow-hidden opacity-[1] ">
-          {/* <div className="absolute inset-0 h-auto w-full overflow-hidden opacity-[.2]">
+        <GradientBackground />
+        <div className="bg-white/[.47] shadow-bg backdrop-blur-[158.26499938964844px]">
+          <Navbar />
+
+          <main className="isolate flex h-full flex-col">{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
+
+{
+  /* <div className="absolute inset-0 h-auto w-full overflow-hidden opacity-[.2]">
           <svg
             // width="1440"
             // height="3759"
@@ -84,68 +89,6 @@ export default function RootLayout({
               ry="805.958"
               fill="#ADD5EB"
             />
-          </svg> */}
-        </div>
-        <div className="bg-white/[.47] shadow-bg backdrop-blur-[158.26499938964844px]">
-          <Navbar />
-          {/* **** */}
-          <main className="isolate flex h-full flex-col">{children}</main>
-          <Footer />
-        </div>
-      </body>
-    </html>
-  );
+          </svg>
+        </div> */
 }
-
-{
-  /* <svg
-  // width="1440"
-  // height="3759"
-  viewBox="0 0 1440 3759"
-  preserveAspectRatio="xMidYMid meet"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
-  className="absolute inset-0 "
->
-  <ellipse
-    cx="375.808"
-    cy="434.354"
-    rx="434.267"
-    ry="865.042"
-    fill="#ADEAEB"
-  />
-  <ellipse
-    cx="1078.25"
-    cy="667.018"
-    rx="578.915"
-    ry="883.331"
-    fill="#ADB3EB"
-  />
-  <ellipse
-    cx="268.007"
-    cy="3035.77"
-    rx="486.173"
-    ry="722.626"
-    fill="#ADEBCB"
-  />
-  <ellipse
-    cx="1096.43"
-    cy="2884.38"
-    rx="428.083"
-    ry="799.011"
-    fill="#ADEBB6"
-  />
-  <ellipse
-    cx="699.168"
-    cy="1632.22"
-    rx="388.683"
-    ry="805.958"
-    fill="#ADD5EB"
-  />
-</svg> */
-}
-
-// bg-light-purple
-// bg-light-blue/[.01]
-// bg-white/[.43] shadow-bg backdrop-blur-[158.26499938964844px]
-// bg-white shadow-bg backdrop-blur-[158.26499938964844px]
