142240c5722681b03abe69ff5611ee095ebd20c6
Ben Chavez <ben.m.chavez@gmail.com>
2023-07-18 23:27:26 -0500

    added content-security-policy

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,14 +1,14 @@
 // app/blog/page.tsx
 
-import type { Metadata } from "next";
-import Link from "next/link";
-import { allBlogs } from "contentlayer/generated";
-import DrawingIcon from "../../components/icons/drawing-icon";
-import Container from "../../components/layout/Container";
-import BlogCard from "../components/blog-card";
-import { Balancer } from "react-wrap-balancer";
-import { notFound } from "next/navigation";
-import { Mdx } from "@/app/components/mdx";
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import { allBlogs } from 'contentlayer/generated';
+import DrawingIcon from '../../components/icons/drawing-icon';
+import Container from '../../components/layout/Container';
+import BlogCard from '../components/blog-card';
+import { Balancer } from 'react-wrap-balancer';
+import { notFound } from 'next/navigation';
+import { Mdx } from '@/app/components/mdx';
 
 interface BlogProps {
   params: {
@@ -27,8 +27,6 @@ export default async function Blog({ params }: BlogProps) {
     <>
       <section>
         <Container className="mx-auto flex flex-col">
-          {/* Header Card */}
-          {/* justify-center */}
           <div className="mt-10  ">
             <Link
               href="/blog"
@@ -100,32 +98,6 @@ export default async function Blog({ params }: BlogProps) {
               </div>
             </div>
           </div>
-          {/* <div className="mt-10 grid grid-cols-1 gap-3 tablet:grid-cols-2 tablet:px-[24px]">
-            {allBlogs
-              .sort((a, b) => {
-                if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-                  return -1;
-                }
-                return 1;
-              })
-              .map((post: any) => (
-                <Link
-                  key={post.slug}
-                  href={`/blog/${post.slug}`}
-                  className="rounded-3xl bg-white/[.68] px-[24px] py-[19px] shadow-sm backdrop-blur-[23px]"
-                >
-                  <div className="flex w-full flex-col">
-                    <h2 className="font-inter text-subtitle text-dark1">
-                      <Balancer>{post.title}</Balancer>
-                    </h2>
-                    <p className="font-open-sans text-fs-lg text-dark1">
-                      {post.publishedAt}
-                    </p>
-                  </div>
-                </Link>
-              ))}
-          </div> */}
-          {/* <div className="mt-10 grid grid-cols-1 gap-3 tablet:grid-cols-2 tablet:px-[24px]"> */}
 
           <div className="mt-10 laptop:px-[40px]">
             <div
@@ -140,154 +112,3 @@ export default async function Blog({ params }: BlogProps) {
     </>
   );
 }
-
-{
-  /* <div className="mb-[4.5rem] w-full bg-blue-500"> */
-}
-{
-  /*  <Container className="px-16"> */
-}
-{
-  /* <Container className="mx-auto flex w-full flex-col items-start gap-y-10  p-5">
-  {/* Header Card */
-}
-//   <div className="flex w-full items-center justify-center rounded-3xl bg-white/[.47] px-32 py-14 backdrop-blur-[23px]">
-//     {/* Content */}
-//     {/* TODO: adjust gap for smaller screensizes: gap-?? bg-teal-500 */}
-//     <div className="flex justify-start gap-[9.375rem]">
-//       <div className="flex flex-col items-start justify-center gap-6 p-2">
-//         <h1 className="font-inter  text-fs-xl text-dark1">Blog</h1>
-//         <p className="font-open-sans text-fs-lg text-dark1">
-//           Welcome to development journey of{' '}
-//           <span className="text-highlight-focus-1">Ctrl-F Plus!</span> Here, you
-//           can find articles about my progress, challenges, and successes. I hope
-//           you enjoy reading about my experiences and find them inspiring.
-//         </p>
-//       </div>
-//       <div className="hidden laptop:block">
-//         <DrawingIcon />
-//       </div>
-//     </div>
-//   </div>
-
-//   <div
-//     // className="grid w-full gap-3  laptop:grid-cols-9"
-//     // className="flex flex-grow flex-wrap content-start items-start gap-3"
-//     className="flex flex-wrap content-start items-start gap-[12px] self-stretch "
-//   >
-//     {blogPosts.map((post) => {
-//       return (
-//         <div
-//           key={post.title}
-//           className="flex flex-col items-start gap-2 rounded-3xl bg-white/[.68] px-[24px] py-[19px] backdrop-blur-[23px]"
-//         >
-//           <div className="flex flex-col items-start gap-[12px] p-2">
-//             <h2 className="font-inter text-subtitle text-dark1">
-//               {post.title}
-//             </h2>
-//             <p className="body-18  font-open-sans text-dark1">{post.date}</p>
-//           </div>
-//         </div>
-//       );
-//     })}
-//   </div>
-// </Container>; */}
-
-// <Container className=" ">
-//   {/*  <Container className="px-16"> */}
-//   <div className="grid grid-cols-2 gap-3  px-16">
-//     {blogPosts.map((post) => {
-//       return (
-//         // <div
-//         //   key={post.title}
-//         //   className="flex flex-col gap-2 rounded-[24px] bg-white px-[24px] py-[19px]"
-//         //   // className="flex flex-col gap-2 space-x-2 rounded-3xl "
-//         //   // className="m-2 flex h-auto w-auto min-w-[553px] flex-col space-x-2 bg-white px-6 py-[19px] first-letter:rounded-xl"
-//         // >
-//         //   <h2 className="font-inter text-subtitle text-dark1">
-//         //     {post.title}
-//         //   </h2>
-//         //   <p className="body-18  font-open-sans text-dark1">{post.date}</p>
-//         // </div>
-
-//         <div
-//           key={post.title}
-//           className="inline-flex h-[123px] w-[553px] flex-col items-start justify-start gap-2 rounded-3xl bg-white bg-opacity-70 px-6 py-[19px] backdrop-blur-[46px]"
-//         >
-//           <div className="flex flex-col items-start justify-start gap-3 p-2">
-//             <div className="text-center text-[23px] font-semibold leading-7 text-neutral-800">
-//               Environment variables and GraphQL queries
-//             </div>
-//             <div className="text-center text-[18px] font-normal leading-relaxed text-neutral-800">
-//               12/11/2023
-//             </div>
-//           </div>
-//         </div>
-//       );
-//     })}
-//   </div>
-// </Container>
-
-//
-//
-//
-// <div className="">
-//   <div className="bg-slate-400 py-24 sm:py-32">
-//     <div className="mx-auto max-w-7xl px-6 lg:px-8">
-//       {/*  */}
-//       {/* <div className="mx-auto max-w-2xl sm:text-center">
-//               <h2 className="text-3xl sm:text-4xl font-bold tracking-tight text-gray-900">
-//                 Meet our leadership
-//               </h2>
-//               <p className="text-lg mt-6 leading-8 text-gray-600">
-//                 We’re a dynamic group of individuals who are passionate about
-//                 what we do and dedicated to delivering the best results for our
-//                 clients.
-//               </p>
-//             </div> */}
-
-//       <div className="flex w-full items-center justify-center rounded-3xl  bg-white/[.47] px-32 py-14 backdrop-blur-[23px]">
-//         {/* Content */}
-//         {/* TODO: adjust gap for smaller screensizes: gap-?? bg-teal-500 */}
-//         <div className="flex justify-start gap-[9.375rem]">
-//           <div className="flex flex-col items-start justify-center gap-6 p-2">
-//             <h1 className="font-inter  text-fs-xl text-dark1">Blog</h1>
-//             <p className="font-open-sans text-fs-lg text-dark1">
-//               Welcome to development journey of{' '}
-//               <span className="text-highlight-focus-1">Ctrl-F Plus!</span> Here,
-//               you can find articles about my progress, challenges, and
-//               successes. I hope you enjoy reading about my experiences and find
-//               them inspiring.
-//             </p>
-//           </div>
-//           <div className="hidden desktop:block">
-//             <DrawingIcon />
-//           </div>
-//         </div>
-//       </div>
-
-//       <ul
-//         role="list"
-//         className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-6 gap-y-3   sm:grid-cols-2 lg:max-w-4xl lg:gap-x-8 xl:max-w-none"
-//       >
-//         {blogPosts.map((post) => (
-//           <li
-//             key={post.title}
-//             className="flex flex-col gap-6 rounded-3xl border border-gray-300 bg-white/[.68] shadow-sm backdrop-blur-[23px] xl:flex-row"
-//           >
-//             {/* gap-2 gap-[12px] p-2 */}
-//             <div className="flex-auto px-[24px] py-[19px] ">
-//               <h3 className="font-inter text-subtitle leading-8 tracking-tight text-dark1 ">
-//                 {post.title}
-//               </h3>
-
-//               <p className="font-open-sans text-fs-lg leading-7 text-dark1">
-//                 {post.date}
-//               </p>
-//             </div>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   </div>
-// </div>;
--- next.config.js
+++ next.config.js
@@ -4,6 +4,69 @@ const { withContentlayer } = require('next-contentlayer');
 const nextConfig = {
   experimental: {},
   reactStrictMode: true,
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
+
+// https://nextjs.org/docs/app/api-reference/next-config-js/headers
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' va.vercel-scripts.com/;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+    // value: 'SAMEORIGIN'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+    //  value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+  },
+  // {
+  //   key: 'X-XSS-Protection',
+  //   value: '1; mode=block',
+  // },
+];
 
 module.exports = withContentlayer(nextConfig);
