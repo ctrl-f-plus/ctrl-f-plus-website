481c5ec051951196c1aaef3de5f9fbdcd50e1561
Ben Chavez <ben.m.chavez@gmail.com>
2023-07-18 23:52:50 -0500

    updates security policy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { withContentlayer } = require('next-contentlayer');
 const nextConfig = {
   experimental: {},
   reactStrictMode: true,
+  poweredByHeader: false,
   headers() {
     return [
       {
