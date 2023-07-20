bbd486b676de9c1278c80cbf4d0973830ab598a1
Ben Chavez <ben.m.chavez@gmail.com>
2023-07-19 22:51:51 -0500

    footer notes

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -3,12 +3,37 @@
 import LogoIconSecondary from '../icons/logo-secondary';
 import Container from './Container';
 
+/**
+ *
+ * footer navigation:
+ *
+ * Download:
+ *  - Chrome
+ *  - Brave
+ *  - Firefox
+ *  - Edge
+ *
+ * Contribute:
+ *  - Open Source
+ *  - Sponsor
+ *
+ * Some Header
+ *  - Features
+ *  - Blog
+ *  - Our Team
+ *
+ * Company:
+ *  - Contact
+ *  - Blog
+ *  - About
+ */
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
 
   return (
-    <footer className="bg-dark1 mt-18 tablet:mt-24 wide:mt-[7.625rem]">
+    <footer className="mt-18 bg-dark1 tablet:mt-24 wide:mt-[7.625rem]">
       <Container className="mx-auto flex h-[5.375rem] w-full items-center justify-between">
         <LogoIconSecondary />
         {/* <p>&copy; 2020 Your Company, Inc. All rights reserved.</p> */}
