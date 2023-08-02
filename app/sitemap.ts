b23994922a8358d91ac0b2cc5050d5d25ea14b41
Ben Chavez <ben.m.chavez@gmail.com>
2023-08-02 00:51:51 -0500

    added static hero animation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,15 +3,16 @@
 import { allBlogs } from 'contentlayer/generated';
 
 export default async function sitemap() {
+    const routes = ['', '/#features', '/blog', '/about'].map((route) => ({
+    url: `https://ctrl-f.plus${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
   const blogs = allBlogs.map((post) => ({
     url: `https://ctrl-f.plus/blog/${post.slug}`,
     lastModified: post.publishedAt,
   }));
 
-  const routes = ['', '/blog'].map((route) => ({
-    url: `https://ctrl-f.plus${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }));
 
   return [...routes, ...blogs];
 }
