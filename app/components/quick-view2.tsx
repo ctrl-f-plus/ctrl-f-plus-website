0732055368310900f1a9297736cc10a094efd86f
Ben Chavez <ben.m.chavez@gmail.com>
2023-08-11 20:01:02 -0500

    fixed linting errors

--- app/components/quick-view2.tsx
+++ app/components/quick-view2.tsx
@@ -7,6 +7,10 @@ import { CSSProperties, useRef, useState } from 'react';
 import Image from 'next/image';
 import ButtonPrimary from '../components/buttons/ButtonPrimary';
 
+type IconProps = {
+  className: string;
+};
+
 function XMarkIcon({ className }: IconProps) {
   return (
     <svg
