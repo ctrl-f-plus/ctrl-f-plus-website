a935c4135fee49229fdbba043706fbdd32523cc1
Ben Chavez <ben.m.chavez@gmail.com>
2023-08-13 23:44:53 -0500

    fixed mobile nav logo link behavior

--- app/components/navbar/mobile-menu.tsx
+++ app/components/navbar/mobile-menu.tsx
@@ -27,10 +27,14 @@ export default function MobileMenu({
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-11 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.5">
+            <CtrlLink
+              href="/"
+              className="-m-1.5 p-1.5"
+              onClick={() => setMobileMenuOpen(false)}
+            >
               <span className="sr-only">Ctrl-F Plus</span>
               <LogoIcon />
-            </Link>
+            </CtrlLink>
 
             <button type="button" onClick={() => setMobileMenuOpen(false)}>
               <span className="sr-only">Close menu</span>
