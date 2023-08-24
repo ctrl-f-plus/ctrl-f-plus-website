a004e93d96ef61689a919744d7f3f1b74b4e254e
Ben Chavez <ben.m.chavez@gmail.com>
2023-08-23 19:03:11 -0500

    updated canvas gradient component

--- app/components/canvas-gradient.tsx
+++ app/components/canvas-gradient.tsx
@@ -7,27 +7,25 @@ function CanvasGradient() {
 
   useEffect(() => {
     const injectGradient = () => {
-      console.log('gradient useEffect');
       import('../lib/gradient').then(({ Gradient }) => {
         const gradientInstance = new Gradient();
+
         //@ts-ignore
         gradientInstance.initGradient('#gradient-canvas');
+
         //@ts-ignore
         setGradient(gradientInstance);
       });
     };
 
-    process.env.NODE_ENV === 'development' && injectGradient();
+    process.env.NODE_ENV !== 'development' && injectGradient();
   }, []);
 
   return (
-    <div>
-      <canvas
-        id="gradient-canvas"
-        // bg-gradient-cyan/50
-        className="absolute h-screen w-screen bg-pink-500"
-      />
-    </div>
+    <canvas
+      id="gradient-canvas"
+      className="absolute h-screen w-screen bg-gradient-cyan/50"
+    />
   );
 }
 
