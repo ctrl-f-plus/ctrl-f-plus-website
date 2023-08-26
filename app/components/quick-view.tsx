2e39c1bb5b6f04c000f02b489bdcb48440982829
Ben Chavez <ben.m.chavez@gmail.com>
2023-08-26 17:33:43 -0500

    temp switch back to GIF

--- app/components/quick-view.tsx
+++ app/components/quick-view.tsx
@@ -96,7 +96,7 @@ export default function QuickView() {
                     <div className="mt-2 h-full w-full overflow-hidden">
                       <div className="relative flex aspect-video h-full w-full items-center justify-center">
                         <div className="flex h-full w-full ">
-                          {/* <Image
+                          <Image
                             ref={initialRef}
                             unoptimized={true}
                             src="https://i.imgur.com/rxhEz0S.gif"
@@ -108,7 +108,7 @@ export default function QuickView() {
                             height={853}
                             aria-hidden="true"
                             priority
-                          /> */}
+                          />
                           {/* <iframe src="https://www.youtube.com/..."></iframe> */}
                           {/* <iframe
                             width="560"
@@ -120,10 +120,7 @@ export default function QuickView() {
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             allowfullscreen
                           ></iframe> */}
-                          <div
-                            // className="flex items-center overflow-hidden shadow-2xl"
-                            // style={{ width: '80vw', height: '80vh' }}
-                            // className="flex h-full w-full items-center overflow-hidden shadow-2xl"
+                          {/* <div
                             className="flex h-[663px] w-[1179px] items-center justify-center overflow-hidden shadow-2xl shadow-blue-500"
                           >
                             <ReactPlayer
@@ -131,7 +128,7 @@ export default function QuickView() {
                               url="https://www.youtube.com/embed/2uBnJsjbHdo?si=xyHg5VTPP4cFOHDT"
                               // playing
                               controls
-                              className=" aspect-video "
+                              className=" aspect-video w-3/4"
                               width="85%"
                               height="auto"
                               // frameborder="0"
@@ -139,7 +136,7 @@ export default function QuickView() {
                               // width="1179"
                               // height="663"
                             />
-                          </div>
+                          </div> */}
                         </div>
                       </div>
                     </div>
