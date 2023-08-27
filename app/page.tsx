35bf33de71aab12c43603ef18a86b224eb39371b
Ben Chavez <ben.m.chavez@gmail.com>
2023-08-26 19:54:36 -0500

    updated loader

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import Loading from './loading';
 export default function Page() {
   const components = [
     <Hero key="hero" />,
-    <Loading key="loading" />,
     <FeaturesHeader key="featuresHeader" />,
     <FeatureCard key="featureCard" />,
     <CallToAction key="callToAction" />,
