9a6d6f90b96fa1b78946d03834e578c57565d6db
Ben Chavez <ben.m.chavez@gmail.com>
2023-09-14 10:28:01 -0500

    dynamic loading of cta and feature cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 // app/page.tsx
 import 'server-only';
 
-import CallToAction from './components/call-to-action';
+// import CallToAction from './components/call-to-action';
 import { FadeInStagger } from './components/fade-in';
-import FeatureCard from './components/feature-card';
+// import FeatureCard from './components/feature-card';
 import FeaturesHeader from './components/features-header';
 import Hero from './components/hero';
+
+import dynamic from 'next/dynamic';
+const FeatureCard = dynamic(() => import('./components/feature-card'));
+const CallToAction = dynamic(() => import('./components/call-to-action'));
 
 export default function Page() {
   const components = [
