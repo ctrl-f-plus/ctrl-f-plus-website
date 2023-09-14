3541b0bea08efea7221818faeccb460e3ea07d63
Ben Chavez <ben.m.chavez@gmail.com>
2023-09-14 12:39:28 -0500

    removed shadow and blur from background and moves css gradient colors to their own file

--- app/components/canvas-gradient.tsx
+++ app/components/canvas-gradient.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import '../../styles/canvas-gradient.css';
 
 function CanvasGradient() {
   const [gradient, setGradient] = useState(null);
