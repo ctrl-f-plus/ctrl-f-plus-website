d3af23ddfad0a1c58fc79c95163ff91a03831d6f
Ben Chavez <ben.m.chavez@gmail.com>
2023-09-14 19:28:49 -0500

    update

--- app/components/canvas-gradient-inner.tsx
+++ app/components/canvas-gradient-inner.tsx
@@ -4,7 +4,7 @@ function CanvasGradientInner() {
   return (
     <>
       <div className="fixed h-screen w-screen bg-white/[.47] shadow-bg" />
-      <canvas id="gradient-canvas" className="fixed h-screen w-screen " />
+      <canvas id="gradient-canvas" className="fixed !h-[2500px] !w-[2500px]" />
     </>
   );
 }
