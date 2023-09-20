e9f7ca06dcbc300484b955d2a8379d33027543fd
Ben Chavez <ben.m.chavez@gmail.com>
2023-09-19 21:53:27 -0500

    background gradient svg version

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
 
 const ContentSecurityPolicy = `
   default-src 'self'  vercel.live;
-  script-src 'self' https://localhost:12719 'unsafe-eval' 'unsafe-inline' va.vercel-scripts.com cdn.vercel-insights.com vercel.live https://www.youtube.com https://imgur.com/ https://code.jquery.com;
+  script-src 'self' https://localhost:12719 'unsafe-eval' 'unsafe-inline' va.vercel-scripts.com cdn.vercel-insights.com vercel.live https://www.youtube.com https://imgur.com/;
   frame-src youtube.com www.youtube.com https://imgur.com/;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
