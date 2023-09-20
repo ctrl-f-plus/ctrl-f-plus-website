f714c1d358f0d7c6732f075770f212c0925aed7c
Ben Chavez <ben.m.chavez@gmail.com>
2023-09-20 02:02:32 -0500

    rever hero animation for mobile

--- app/components/icons/hero-animation.tsx
+++ app/components/icons/hero-animation.tsx
@@ -9,53 +9,49 @@ import { useEffect, useState } from 'react';
 
 export default function HeroAnimation() {
   const prefersReducedMotion = useReducedMotion();
-  // const [isDesktop, setIsDesktop] = useState(false); // State to track device type
+  const [isDesktop, setIsDesktop] = useState(false); // State to track device type
 
-  // useEffect(() => {
-  //   // Function to check and set if the device is desktop
-  //   const checkDeviceType = () => {
-  //     if (window.innerWidth >= 768) {
-  //       // Assuming 1024px as the breakpoint for desktop
-  //       setIsDesktop(true);
-  //     } else {
-  //       setIsDesktop(false);
-  //     }
-  //   };
+  useEffect(() => {
+    // Function to check and set if the device is desktop
+    const checkDeviceType = () => {
+      if (window.innerWidth >= 768) {
+        // Assuming 1024px as the breakpoint for desktop
+        setIsDesktop(true);
+      } else {
+        setIsDesktop(false);
+      }
+    };
 
-  //   // Initial check
-  //   checkDeviceType();
+    // Initial check
+    checkDeviceType();
 
-  //   // Add event listener for window resize
-  //   window.addEventListener('resize', checkDeviceType);
+    // Add event listener for window resize
+    window.addEventListener('resize', checkDeviceType);
 
-  //   // Cleanup listener on component unmount
-  //   return () => {
-  //     window.removeEventListener('resize', checkDeviceType);
-  //   };
-  // }, []);
+    // Cleanup listener on component unmount
+    return () => {
+      window.removeEventListener('resize', checkDeviceType);
+    };
+  }, []);
 
-  // if (!isDesktop) return null;
+  if (!isDesktop) return null;
   // const imageSrc = prefersReducedMotion ? heroAnimationStill : gifHero;
 
-  // let imageSrc, className;
-  // if (prefersReducedMotion) {
-  //   imageSrc =
-  //     'https://imagedelivery.net/iiP7mcx9iKOhYG4rdM-BWg/2d289852-4c4e-4bc4-a123-58255d587700/variant1';
-  //   className = '-mt-9';
-  // } else {
-  //   imageSrc =
-  //     // 'https://res.cloudinary.com/dyy8g76av/image/upload/v1695149280/gif-hero-animated_pwcfif.webp';
-  //     // 'https://res.cloudinary.com/dyy8g76av/image/upload/v1695149280/gif-hero-animated_pwcfif.webp';
-  //     'https://res.cloudinary.com/dyy8g76av/image/upload/f_auto,fl_animated,q_90/v1695149280/gif-hero-animated_pwcfif.webp';
-  //   // 'https://res.cloudinary.com/dyy8g76av/image/upload/f_auto:video,q_100/v1695149280/gif-hero-animated_kuadwt.webp';
-  //   // 'https://imagedelivery.net/iiP7mcx9iKOhYG4rdM-BWg/bac1a18f-e104-4a26-95ec-b303f4550300/public';
-  //   // 'https://imagedelivery.net/iiP7mcx9iKOhYG4rdM-BWg/bac1a18f-e104-4a26-95ec-b303f4550300/variant1';
-  //   className = '-mt-18';
-  // }
-
-  const className = prefersReducedMotion
-    ? '-mt-9 hidden laptop:block'
-    : '-mt-18 hidden laptop:block';
+  let imageSrc, className;
+  if (prefersReducedMotion) {
+    imageSrc =
+      'https://imagedelivery.net/iiP7mcx9iKOhYG4rdM-BWg/2d289852-4c4e-4bc4-a123-58255d587700/variant1';
+    className = '-mt-9';
+  } else {
+    imageSrc =
+      // 'https://res.cloudinary.com/dyy8g76av/image/upload/v1695149280/gif-hero-animated_pwcfif.webp';
+      // 'https://res.cloudinary.com/dyy8g76av/image/upload/v1695149280/gif-hero-animated_pwcfif.webp';
+      'https://res.cloudinary.com/dyy8g76av/image/upload/f_auto,fl_animated,q_90/v1695149280/gif-hero-animated_pwcfif.webp';
+    // 'https://res.cloudinary.com/dyy8g76av/image/upload/f_auto:video,q_100/v1695149280/gif-hero-animated_kuadwt.webp';
+    // 'https://imagedelivery.net/iiP7mcx9iKOhYG4rdM-BWg/bac1a18f-e104-4a26-95ec-b303f4550300/public';
+    // 'https://imagedelivery.net/iiP7mcx9iKOhYG4rdM-BWg/bac1a18f-e104-4a26-95ec-b303f4550300/variant1';
+    className = '-mt-18';
+  }
 
   return (
     <>
@@ -63,8 +59,8 @@ export default function HeroAnimation() {
         <Image
           height="423"
           width="480"
-          // src={imageSrc}
-          src="https://res.cloudinary.com/dyy8g76av/image/upload/f_auto,fl_animated,q_90/v1695149280/gif-hero-animated_pwcfif.webp"
+          src={imageSrc}
+          // src=""
           // src="https://res.cloudinary.com/dyy8g76av/image/upload/v1695149280/gif-hero-animated_pwcfif.webp"
           alt={'Animated Search Visual'}
           aria-hidden="true"
